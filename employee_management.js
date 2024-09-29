b11c82065dc2e1fb061abfb87bf4ce151b507e5e
Siddhartha Konduru <kondurus@usf.edu>
2024-09-29 19:58:18 -0400

    Create Employee class with properties and methods

--- employee_management.js
+++ employee_management.js
@@ -1,1 +1,16 @@
-cl
+class Employee {
+    constructor(name, salary, position, department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+    getDetails() {
+        return `Name of employee: ${this.name}, Position of employee: ${this.position}, Salary of employee: $${this.salary}`;
+    }
+}
+const jacob = new Employee("Jacob", 84000, "Designer", "Marketing");
+const olivia = new Employee("Olivia", 77000, "Developer", "Engineering");
+
+console.log(jacob.getDetails()); 
+console.log(olivia.getDetails());   
