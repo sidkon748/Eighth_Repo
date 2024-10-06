4e181f33be6e42c900fa7043cc7a1e36027daac9
Siddhartha Konduru <kondurus@usf.edu>
2024-10-06 12:04:14 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -1,47 +1,78 @@
+//Task 1
+//Create an Employee Class
 class Employee {
-    constructor(name, salary, position, department) {
+    // Class Employee created with properties name, salary, position & department
+    constructor(name, salary, position, department) { 
         this.name = name;
         this.salary = salary;
         this.position = position;
         this.department = department;
     }
-    getDetails() {
+    // getDetails method for printing employee name, position, and salary 
+    getDetails() { 
         return `Name of employee: ${this.name}, Position of employee: ${this.position}, Salary of employee: $${this.salary}`;
     }
 }
-const jacob = new Employee("Jacob", 84000, "Designer", "Marketing");
-const olivia = new Employee("Olivia", 77000, "Developer", "Engineering");
+
+//Task 3
+//Create a Manager Class that Inherits from Employee
+class Manager extends Employee { 
+    // child class Manager inherents from employee parent class
+    constructor(name, salary, position, department, bonus) { 
+        // Adds bonus property to manager class
+        super(name, position, salary, department);
+        this.bonus = bonus;
+        this.position = position;
+        this.salary=salary;
+    }
+    // getDetails method for printing manager's bonus in addition to properties from Employee class
+    getDetails() { 
+        return `${super.getDetails()}, Bonus: $${this.bonus}`;
+    }
+}
+
+// Four employee & manager instances with name, salary, and departments properties
+const jacob = new Manager("Jacob", 84000, "Designer", "Marketing");
+const olivia = new Manager("Olivia", 77000, "Developer", "Engineering");
 const sophie = new Employee("Sophie", 84000, "Developer", "Marketing");
 const mark = new Employee("Mark", 77000, "Designer", "Engineering");
 
+// Logs employee properties with getDetails method 
 console.log(jacob.getDetails()); 
 console.log(olivia.getDetails());   
 console.log(sophie.getDetails()); 
 console.log(mark.getDetails()); 
 
+//Task 2
+//Create a Department Class
 class Department {
+    // class Department with name property and employee properties within Department class
     constructor(name, employees){
         this.name = name;
         this.employees = [];
 
     }
+    // addEmployee method adds employees to an employees array
     addEmployee(employee) {
         this.employees.push(employee);
     }
-
+    // getDepartmentSalary method to calculate total of all employees in a departent salaries with reduce function
     getDepartmentSalary() {
         return this.employees.reduce((total, employee) => total + employee.salary, 0);
     }
 
 }
 
+// Defines two instances of class Department for employees with two name properties 
 const marketing = new Department("Marketing");
 const engineering = new Department("Engineering");
 
+// Adds all salaries for each department class
 marketing.addEmployee(jacob);
 marketing.addEmployee(sophie);
 engineering.addEmployee(olivia);
 engineering.addEmployee(mark);
 
-console.log(`Total salary in ${marketing.name} department: $${marketing.getDepartmentSalary()}`);
-console.log(`Total salary in ${engineering.name} department: $${engineering.getDepartmentSalary()}`);
+// Logs Department total salries of each department
+console.log(`Total salary in Marketing department: $${marketing.getDepartmentSalary()}`);
+console.log(`Total salary in Engineering department: $${engineering.getDepartmentSalary()}`);
